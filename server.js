6cf4f7223787c3f0ab463757da34893c8a497457
UmeshChandra <chinturapaka452@hotmail.com>
2021-06-19 18:34:47 +0530

    set port as process.env.PORT

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ fastify.register(require("./routes/main.route"));
 
 const start = async () => {
   try {
-    await fastify.listen(5000);
+    await fastify.listen(process.env.PORT || 5000);
   } catch (err) {
     fastify.log.error(err);
     process.exit(1);
